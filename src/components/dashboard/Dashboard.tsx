0a97b7614239ce1297c187f4fd76b977da8fcfdc
Sachin Khadka <sachinworkk@gmail.com>
2022-12-09 12:43:44 +0545

    Add description in Dashboard

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -3,7 +3,15 @@ import Sidebar from "../commons/Sidebar";
 function Dashboard() {
   return (
     <Sidebar>
-      <h1>Dashboard</h1>
+      <h1>
+        Welcome to the Vaccine Management System! This system allows users to
+        easily manage their vaccine records, such as adding, editing, viewing,
+        and deleting vaccines. It is designed to provide a comprehensive and
+        secure way to store and manage vaccine information for individuals and
+        organizations. With this system, users can easily track their vaccine
+        inventory. It is easy to use and provides a safe and secure environment
+        for managing vaccine records.
+      </h1>
     </Sidebar>
   );
 }
